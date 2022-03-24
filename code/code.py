2553f65eb596e257c606af425e4bedcfd6f33433
hezy29 <67768189+hezy29@users.noreply.github.com>
2022-03-24 17:39:41 +0800

    Update code.py

--- code/code.py
+++ code/code.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
+from semopy import Model
+import desc
 
 # Load data
 data = pd.read_excel('./data/data.xlsx')
+data['time_used'] = data['time_used'].apply(lambda x: int(str(x)[:-1]))
 
 age = pd.read_excel('./data/age.xlsx')
 age.index = np.arange(1, len(age)+1)
@@ -15,6 +18,9 @@ prof.index = np.arange(1, len(prof)+1)
 
 # Data dictionary
 data_dict = dict(age.to_dict(), **edu.to_dict(), **prof.to_dict())
-
 data.replace(data_dict, inplace=True)
-print(data.head())
+
+# Model
+desc_1 = desc.mod1
+mod_1 = Model(description=desc_1)
+mod_1.fit(data)
