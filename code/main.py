d166ca0fdcbd5b6dd34811292d39b273ee0264cd
hezy29 <67768189+hezy29@users.noreply.github.com>
2022-03-27 16:09:54 +0800

    Modify python file name
    Due to the conflict with duplicate name in the imported module.

--- code/main.py
+++ code/main.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import numpy as np
 from semopy import Model
+from semopy import semplot
+import os
 import desc
 
 # Load data
-data = pd.read_excel('./data/data.xlsx')
+data = pd.read_excel('./PoHC/data/data.xlsx')
 data['time_used'] = data['time_used'].apply(lambda x: int(str(x)[:-1]))
 
-age = pd.read_excel('./data/age.xlsx')
+age = pd.read_excel('./PoHC/data/age.xlsx')
 age.index = np.arange(1, len(age)+1)
 
-edu = pd.read_excel('./data/education.xlsx')
+edu = pd.read_excel('./PoHC/data/education.xlsx')
 edu.index = np.arange(1, len(edu)+1)
 
-prof = pd.read_excel('./data/profession.xlsx')
+prof = pd.read_excel('./PoHC/data/profession.xlsx')
 prof.index = np.arange(1, len(prof)+1)
 
 # Data dictionary
@@ -24,3 +26,6 @@ data.replace(data_dict, inplace=True)
 desc_1 = desc.mod1
 mod_1 = Model(description=desc_1)
 mod_1.fit(data)
+
+# SEM plot
+semplot(mod_1, "./PoHC/picture/mod_1.svg")
