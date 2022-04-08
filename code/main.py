06f826b177380d46a148d87ff1c4076429d17b38
hezy29 <67768189+hezy29@users.noreply.github.com>
2022-04-08 23:32:16 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-from semopy import Model
-from semopy import semplot
+from semopy import Model, semplot, calc_stats
 import desc
 import pingouin as pg
 from factor_analyzer.factor_analyzer import calculate_kmo, calculate_bartlett_sphericity
@@ -22,6 +21,13 @@ prof.index = np.arange(1, len(prof)+1)
 # Data dictionary
 data_dict = dict(age.to_dict(), **edu.to_dict(), **prof.to_dict())
 data.replace(data_dict, inplace=True)
+data.drop(columns=['anti_robot'], inplace=True)
+
+# Columns to inverse
+inv_col = ['doubt_hc', 'govern_viola_priv', 'unnece',
+           'sec_hand_used', 'viola_priv', 'diss_manda_hc', 'dislike_hc']
+data[inv_col] = data[inv_col].apply(lambda x: 5-x)
+
 
 # Reliability
 cronbach_alpha_all, cronbach_alpha_each = pg.cronbach_alpha(
@@ -34,9 +40,20 @@ chi2, p = calculate_bartlett_sphericity(data[data.columns[10:]])
 validity = [kmo_model, chi2]
 
 # Model
-desc_1 = desc.mod_1
-mod_1 = Model(description=desc_1)
-mod_1.fit(data)
+desc_basic = desc.mod_basic
+mod_basic = Model(description=desc_basic)
+mod_basic.fit(data)
+
+desc_fixed = desc.mod_fixed
+mod_fixed = Model(description=desc_fixed)
+mod_fixed.fit(data)
+
+# Fit indices
+stats = calc_stats(model=mod_fixed)
+stats.T.to_excel("./PoHC/out/fit_indices.xlsx")
 
 # SEM plot
-pic_1 = semplot(mod_1, "./PoHC/picture/mod_1.svg")
+mod_basic_no_params = semplot(
+    mod_basic, "./PoHC/picture/mod_basic_no_params.pdf", plot_ests=False)
+mod_basic_with_params = semplot(
+    mod_basic, "./PoHC/picture/mod_basic_with_params.pdf", plot_ests=True, std_ests=True)
