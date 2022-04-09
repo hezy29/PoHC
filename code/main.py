bbd85d4876226080eb409049d2f55678b776fadc
hezy29 <67768189+hezy29@users.noreply.github.com>
2022-04-09 18:38:45 +0800

    Chinese version of SEM update

--- code/main.py
+++ code/main.py
@@ -18,16 +18,19 @@ edu.index = np.arange(1, len(edu)+1)
 prof = pd.read_excel('./PoHC/data/profession.xlsx')
 prof.index = np.arange(1, len(prof)+1)
 
-# Data dictionary
-data_dict = dict(age.to_dict(), **edu.to_dict(), **prof.to_dict())
-data.replace(data_dict, inplace=True)
-data.drop(columns=['anti_robot'], inplace=True)
+header = pd.read_excel('./PoHC/data/header.xlsx')
 
 # Columns to inverse
 inv_col = ['doubt_hc', 'govern_viola_priv', 'unnece',
            'sec_hand_used', 'viola_priv', 'diss_manda_hc', 'dislike_hc']
 data[inv_col] = data[inv_col].apply(lambda x: 5-x)
 
+
+# Data dictionary
+data.columns = header.loc[1].values
+data_dict = dict(age.to_dict(), **edu.to_dict(), **prof.to_dict())
+data.replace(data_dict, inplace=True)
+data.drop(columns=['anti_robot'], inplace=True)
 
 # Reliability
 cronbach_alpha_all, cronbach_alpha_each = pg.cronbach_alpha(
@@ -40,11 +43,11 @@ chi2, p = calculate_bartlett_sphericity(data[data.columns[10:]])
 validity = [kmo_model, chi2]
 
 # Model
-desc_basic = desc.mod_basic
+desc_basic = desc.mod_basic_chi
 mod_basic = Model(description=desc_basic)
 mod_basic.fit(data)
 
-desc_fixed = desc.mod_fixed
+desc_fixed = desc.mod_fixed_chi
 mod_fixed = Model(description=desc_fixed)
 mod_fixed.fit(data)
 
